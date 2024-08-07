9e4354adc7131c5a83736c4547ce0cf8b345811c
AlissonDuarte <alissonsilvamorimduarte@gmail.com>
2024-08-07 00:49:55 -0300

    deploy automatico do contrato

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -2,14 +2,25 @@
 pragma solidity ^0.8.13;
 
 contract Transaction {
-    string public addrs;
+    string public teste = "alo";
+    uint256 public constant price = 100 ether; // 2 ETH
+    address payable public constant sellerAddress = payable(0x114014b1277c6872E0E386c0560B92b3B93E9B04);
+    address public constant buyerAddress = 0xC20E79db64b11A7f3d3954f6aD3c39f055EeC985;
 
-    constructor(string memory _addrs) {
-        addrs  = _addrs;
+    function getAddressAccountValues() public view returns (uint256) {
+        return buyerAddress.balance;
     }
 
-    function readAddress() public view returns (string memory) {
-        return addrs;
+    function sendEther() public payable {
+        require(msg.sender == buyerAddress, "Only the buyer can send Ether");
+        require(msg.value == price, "Incorrect Ether value sent");
+
+        // Transfer the ETH to the seller
+        (bool sent, ) = sellerAddress.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
     }
+    function setVariable(string calldata newValue) public returns (string memory) {
+        teste = newValue;
+        return teste;
+    } 
 }
-
